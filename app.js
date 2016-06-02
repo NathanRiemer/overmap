d5da7a6f4ebb086eb60796e301ec649eb83e831d
Nathan Riemer <nathan.riemer@gmail.com>
2016-06-02 11:54:05 -0400

    Adds in RunKeeper authentication, runkeeper mapping is in development

--- app.js
+++ app.js
@@ -113,5 +113,85 @@ app.get('/api/activities', function(req, res) {
   }
 });
 
+// Runkeeper
+app.get('/sign_in_with_runkeeper', function(req, res) {
+  var state = crypto.randomBytes(24).toString('hex');
+  req.session.rkState = state;
+  res.redirect('https://runkeeper.com/apps/authorize?client_id=' + process.env.RUNKEEPER_OAUTH_CLIENT_ID + '&redirect_uri=http://localhost:3000/oauth/runkeeper&response_type=code&state=' + state);
+});
+
+app.get('/oauth/runkeeper', function(req, res) {
+  var state = req.session.rkState;
+  var code = req.query.code;
+  if (state === req.query.state) {
+    var body = {
+      grant_type: 'authorization_code',
+      client_id: process.env.RUNKEEPER_OAUTH_CLIENT_ID,
+      client_secret: process.env.RUNKEEPER_OAUTH_CLIENT_SECRET,
+      code: code,
+      redirect_uri: 'http://localhost:3000/oauth/runkeeper'
+    }
+    request.post('https://runkeeper.com/apps/token', {form: body}, function(error, httpResponse, responseBody) {
+        var responseBody = JSON.parse(responseBody);
+        req.session.runkeeper_access_token = responseBody.access_token;
+        console.log(responseBody);
+        res.redirect('/');
+    });
+  } else {
+    //abort
+    res.redirect('/');
+  }
+});
+
+app.get('/rk_map', function(req, res) {
+  var loggedIn = !!req.session.runkeeper_access_token;
+
+  if (loggedIn) {
+    var gmap = {src: "http://maps.google.com/maps/api/js?libraries=geometry&key=" + process.env.GOOGLE_MAPS_API_KEY}
+    res.render('rk_map', gmap);
+  } else {
+    res.redirect('/');
+  }
+});
+
+app.get('/api/rk_activities', function(req, res) {
+  var options = {
+    url: 'https://api.runkeeper.com/fitnessActivities/799263817?access_token=' + req.session.runkeeper_access_token
+  };
+  request.get(options, function(error, httpResponse, responseBody) {
+    var responseBody = JSON.parse(responseBody);
+    console.log(responseBody);
+  });
+
+  // var getActivities = function(lines, pageNumber, accessToken) {
+  //   var url = 'https://www.strava.com/api/v3/athlete/activities?per_page=200&access_token=' + accessToken + '&page=';
+  //   var options = {
+  //     url: url + pageNumber
+  //   };
+  //   request.get(options, function(error, httpResponse, responseBody) {
+  //     var responseBody = JSON.parse(responseBody);
+  //     var pLines = responseBody.map(function(activity) {
+  //       return activity.map.summary_polyline;
+  //     });
+  //     Array.prototype.push.apply(lines, pLines);
+  //     if (pLines.length < 200) {
+  //       res.json(lines);
+  //     } else {
+  //       getActivities(lines, ++pageNumber, accessToken);
+  //     }
+  //   });
+  // };
+
+  // var loggedIn = !!req.session.strava_access_token;
+
+  // if (loggedIn) {
+  //   var lines = [];
+  //   var pageNumber = 1;
+  //   getActivities(lines, pageNumber, req.session.strava_access_token);
+  // } else {
+  //   res.redirect('/');
+  // }
+});
+
 
 app.listen(3000);
